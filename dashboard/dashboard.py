2a439da70cfe3a3f00d82446ecd68a5d9f53afae
mikhail-karim <mikhailsakarim@gmail.com>
2024-10-02 22:32:10 +0700

    minor changes

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -7,6 +7,7 @@ from babel.numbers import format_currency
 sns.set(style='dark')
 
 def weather_effect_on_shares(df):
+    # grouping total pengguna bike sharing, suhu cuaca, dan kelembapan berdasarkan durasi waktu
     daily_weather_df = df.groupby('date').agg({
         'total': 'sum', 
         'normal_temp': 'mean',
@@ -17,6 +18,7 @@ def weather_effect_on_shares(df):
 
 
 def shares_on_unsuitable_weather(df):
+    # grouping total pengguna bike sharing berdasarkan jenis kondisi cuaca
     unsuitable_weather_df = main_hour_df.groupby('weather_type').agg({
         'total': 'sum'
     }).reset_index()
@@ -27,6 +29,7 @@ def shares_on_unsuitable_weather(df):
 
 
 def shares_on_different_day_types(df):
+    # grouping total pengguna bike sharing berdasarkan tipe hari
     day_type_df = df.groupby('day_type').agg({
         'total': 'sum'
     }).reset_index()
@@ -40,6 +43,7 @@ def shares_on_different_day_types(df):
 
 
 def shares_by_the_hours(df):
+    # grouping total pengguna bike sharing berdasarkan per-jam
     hourly_rentals_df = df.groupby('hour').agg({
         'total': 'sum'
     }).reset_index()
@@ -48,12 +52,11 @@ def shares_by_the_hours(df):
     
     return hourly_rentals_df
 
-# Load cleaned data
+# memuat data bersih
 main_day_df = pd.read_csv("main_day.csv")
 main_hour_df = pd.read_csv("main_hour.csv")
 
 with st.sidebar:
-    # Menambahkan logo perusahaan
     st.image("Bicycle.png")
     st.markdown("""
     Name: Mikhail Shams Afzal Karim\n
@@ -61,16 +64,16 @@ with st.sidebar:
     ID Dicoding: mikhailkarim2004\n
     """)
 
-# Convert date column to datetime
+# mengubah tipe data date ke datetime
 main_day_df['date'] = pd.to_datetime(main_day_df['date'])
 
-# Prepare dataframes for charts
+# menyiapkan dataframes untuk charts
 daily_weather_df = weather_effect_on_shares(main_day_df)
 unsuitable_weather_df = shares_on_unsuitable_weather(main_day_df)
 day_type_df = shares_on_different_day_types(main_day_df)
 hourly_rentals_df = shares_by_the_hours(main_hour_df)
 
-# Dashboard header
+# dashboard header
 st.header(':sparkles: Bike Sharing Data Analysis :sparkles:')
 
 col1, col2, col3 = st.columns(3)
@@ -86,7 +89,7 @@ with col3:
     total_casual = main_day_df['casual'].sum()
     st.metric("Casual Users", value=total_casual)
 
-# 1. Total Bike Share Usage Over Time
+# Question 1. Total Bike Share Usage Over Time
 st.subheader('Total Bike Share Usage Over Time')
 fig1, ax1 = plt.subplots(figsize=(12, 6))
 ax1.plot(daily_weather_df['date'], daily_weather_df['total'], label='Total Bike Rentals', color='blue')
@@ -97,7 +100,7 @@ ax1.legend()
 ax1.grid(True)
 st.pyplot(fig1)
 
-# 2. Average Temperature Over Time
+# Question 1. Average Temperature Over Time
 st.subheader('Average Temperature Over Time')
 fig2, ax2 = plt.subplots(figsize=(12, 6))
 ax2.plot(daily_weather_df['date'], daily_weather_df['normal_temp'], label='Average Temperature (°C)', color='orange')
@@ -108,7 +111,7 @@ ax2.legend()
 ax2.grid(True)
 st.pyplot(fig2)
 
-# 3. Average Humidity Over Time
+# Question 1. Average Humidity Over Time
 st.subheader('Average Humidity Over Time')
 fig3, ax3 = plt.subplots(figsize=(12, 6))
 ax3.plot(daily_weather_df['date'], daily_weather_df['normal_humidity'], label='Average Humidity (%)', color='green')
@@ -123,27 +126,23 @@ st.markdown(f"""
     These charts shows that the changes in weather conditions such as temperature and humidity does NOT have any significant impact, if any, on the bike sharing usage.
     """, unsafe_allow_html=True)
 
-# 4. Bike Rentals Under Unsuitable Weather Conditions
+# Question 2. Bike Rentals Under Unsuitable Weather Conditions
 st.subheader('Bike Sharing Under Unsuitable Weather Conditions')
 fig4, ax4 = plt.subplots(figsize=(12, 6))
-
-# Ensure you are plotting total rentals per weather condition
 sns.barplot(data=unsuitable_weather_df, x='weather_type', y='total', ax=ax4)
 
-# Set labels and title
 ax4.set_xlabel('Weather Condition')
 ax4.set_ylabel('Total Rentals')
 ax4.set_title('Total Rentals During Unsuitable Weather Conditions')
 ax4.set_ylim(1000, 800000)
 
-# Show the plot
 st.pyplot(fig4)
 
 st.markdown(f"""
     This charts shows that under unsuitable weather condition, bike sharing usage still sees relatively high rates as long as it's a non-stormy weather condition.
     """, unsafe_allow_html=True)
 
-# 5. Bike Sharing Usage by Day Type
+# Question 3. Bike Sharing Usage by Day Type
 st.subheader('Bike Sharing Usage by Day Type')
 fig5, ax5 = plt.subplots(figsize=(6, 6))
 ax5.pie(day_type_df['total'], labels=day_type_df['day_type'], autopct='%1.1f%%', startangle=90)
@@ -155,13 +154,13 @@ st.markdown(f"""
     This charts shows that bike sharing are most often during weekends.
     """, unsafe_allow_html=True)
 
-# 6. Bike Rentals by Hour of the Day
+# Question 4. Bike Rentals by Hour of the Day
 st.subheader('Bike Rentals by Hour of the Day')
 fig6, ax6 = plt.subplots(figsize=(16, 6))
 fig7, ax7 = plt.subplots(figsize=(16, 6))
 
 
-# Total Rentals by Hour
+# total rentals by hour
 sns.barplot(x='hour', y='total', data=hourly_rentals_df, ax=ax6, color='blue')
 ax6.set_xlabel('Hour of the Day')
 ax6.set_ylabel('Total Rentals')
@@ -169,7 +168,7 @@ ax6.set_title('Total Bike Rentals by Hour of the Day')
 
 st.pyplot(fig6)
 
-# Average Rentals by Hour
+# average rentals by hour
 sns.barplot(x='hour', y='average', data=hourly_rentals_df, ax=ax7, color='orange')
 ax7.set_xlabel('Hour of the Day')
 ax7.set_ylabel('Average Rentals')
